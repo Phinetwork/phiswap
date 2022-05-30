224ac7d5cdcccfe6a5c8f6bb5950cbeb64d8ef62
PHI NETWORK <85964511+Phinetwork@users.noreply.github.com>
2022-05-29 18:34:54 -0700

    Add files via upload

---
+++ src/utils/useGetCakeBusdLpPrice.ts
@@ -0,0 +1,16 @@
+import { useCurrency } from 'hooks/Tokens'
+import { useTradeExactIn } from 'hooks/Trades'
+import { tryParseAmount } from 'state/swap/hooks'
+
+const useGetCakeBusdLpPrice = () => {
+  const cakeAddress = '0xf64d866730a41d6af2caf0c9345f7b3f21337668'
+  const busdAddress = '0xc9b0fCB7B97CFC0F1c3C9E360383CCc1Bea6C795'
+  const inputCurrency = useCurrency(cakeAddress)
+  const outputCurrency = useCurrency(busdAddress)
+  const parsedAmount = tryParseAmount('1', inputCurrency ?? undefined)
+  const bestTradeExactIn = useTradeExactIn(parsedAmount, outputCurrency ?? undefined)
+  const price = bestTradeExactIn?.executionPrice.toSignificant(6)
+  return price ? parseFloat(price) : undefined
+}
+
+export default useGetCakeBusdLpPrice
